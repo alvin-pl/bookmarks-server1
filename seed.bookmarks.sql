8e8070ea87fe82dd63498907fb1f49c87316e833
Alvin <alvinpl21@gmail.com>
2020-02-11 07:14:13 -0500

    updated sql

---
+++ seed.bookmarks.sql
@@ -0,0 +1,13 @@
+INSERT INTO bookmarks
+    (title, url, rating, description)
+VALUES
+    ('Google', 'http://www.google.com', '3', 'Internet-related services and products.'),
+    ('Thinkful', 'http://www.thinkful.com', '5', ''),
+    ('Github', 'http://www.github.com', '4', 'Brings together the largest community of developers.'),
+    ('React', 'https://reactjs.org/', '5', 'A JavaScript library for building user interfaces'),
+    ('Nodejs', 'https://nodejs.org/en/', '3', 'As an asynchronous event-driven JavaScript runtime, Node.js is designed to build scalable network applications.'),
+    ('Stack overflow', 'https://stackoverflow.com/', '5', 'We build products that empower developers and connect them to solutions that enable productivity, growth, and discovery.'),
+    ('MDN', 'https://developer.mozilla.org/en-US/', '3', 'jQuery is a fast, small, and feature-rich JavaScript library'),
+    ('JQuery', 'https://jquery.com/', '3', 'Resources for developers, by developers.'),
+    ('W3', 'https://www.w3schools.com/', '5', 'The language for building web pages'),
+    ('PostgreSQL', 'https://www.postgresql.org/', '4', 'The Most Advanced Open Source Relational Database.');
